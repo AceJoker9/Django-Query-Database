d4c5e5f204a80f25514940c1f24c58b3f89bbb00
unknown <neilmagic25@gmail.com>
2023-03-16 04:18:36 -0400

    Finishing up finally!

--- django_answers.py
+++ django_answers.py
@@ -11,7 +11,23 @@ def problem_two(request):
   
   
 
-  #Question 2: 
+#Question 2: 
+def problem_two(request):
+  student = Instructor.objects.filter(hire_date__lt = '2010-01-01').order_by('hire_date')
+  for student in student:
+      print(f'Full Name: {Instructor.first_name} {Instructor.last_name} Hire Date: {Instructor.hire_date}')
+  
+  
+           
+      return complete(request)
+
+
+
+
+
+
+
+#Quesion 3:
 
   def problem_three(request):
     teacher = Instructor.objects.get(id=2)
@@ -20,4 +36,31 @@ def problem_two(request):
               f' Instructor Name: {teacher}, Courses: {shared_course}')
     return complete(request)
   
-  
+
+  #Question 4:
+
+  def problem_four(request):
+  kids = Student.objects.count()
+  classes = Course.objects.count()
+  professors = Instructor.objects.count()
+
+  print(kids, classes, professors)
+  return complete(request)
+
+#Question 5: 
+
+def problem_five(request):
+    new_student = Student.objects.create(Student.first_name("Cornelius"),Student.last_name("Phillips"), Student.year("2023", Student.gpa("4.0")) )
+    return complete(request)
+
+#Question 6:
+
+def problem_six(request):
+    new_update = Student.objects.get(pk=11).update(Student.gpa("4.2"))
+    new_student = Student.objects.get(pk=11)
+    print(new_student)
+    # Make sure to set this equal to the primary key of the row you just created!
+    student_id = 11
+
+    return complete(request)
+
